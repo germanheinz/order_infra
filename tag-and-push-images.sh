871eb551502327d32eee5278a02d74fe1b2f3d41
germanheinz <heinz.german@gmail.com>
2025-08-06 00:31:33 +0200

    Infra changes

--- tag-and-push-images.sh
+++ tag-and-push-images.sh
@@ -1,10 +1,10 @@
 gcloud auth login
 
-docker tag com.order.system/order.service:$1 europe-west4-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/order.service:$1
-docker tag com.order.system/payment.service:$1 europe-west4-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/payment.service:$1
-docker tag com.order.system/stock.service:$1 europe-west4-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/stock.service:$1
+docker tag com.order.system/order.service:$1 europe-southwest1-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/order.service:$1
+docker tag com.order.system/payment.service:$1 europe-southwest1-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/payment.service:$1
+docker tag com.order.system/stock.service:$1 europe-southwest1-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/stock.service:$1
 
-docker push europe-west4-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/order.service:$1
-docker push europe-west4-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/payment.service:$1
-docker push europe-west4-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/stock.service:$1
+docker push europe-southwest1-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/order.service:$1
+docker push europe-southwest1-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/payment.service:$1
+docker push europe-southwest1-docker.pkg.dev/acoustic-atom-468009-u9/order-system-repository/stock.service:$1
 
