891620cf200c058ac0f49505dea85aee5156b275
germanheinz <heinz.german@gmail.com>
2025-08-05 11:37:20 +0200

    Infra changes

--- create-topics.sh
+++ create-topics.sh
@@ -1,10 +1,9 @@
-#local-confluent-kafka-cp-zookeeper-headless
 kafka-topics --zookeeper $1:2181 --topic payment-request --delete --if-exists
 kafka-topics --zookeeper $1:2181 --topic payment-response --delete --if-exists
 kafka-topics --zookeeper $1:2181 --topic stock-approval-request --delete --if-exists
 kafka-topics --zookeeper $1:2181 --topic stock-approval-response --delete --if-exists
 
-kafka-topics --zookeeper $1:2181 --topic payment-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic payment-response --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic stock-approval-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic stock-approval-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic payment-request --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic payment-response --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic stock-approval-request --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic stock-approval-response --create --partitions 3 --replication-factor 1 --if-not-exists
